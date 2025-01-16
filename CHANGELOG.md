e31a761f3c1545f05eb5a9d925d29ce4dfbd3bca
Max Muermann <max@muermann.org>
2025-01-16 21:20:36 +1100

    Project cleanup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,3 @@
 # Change Log
 
 All notable changes to the "erbalist" extension will be documented in this file.
-
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
-## [Unreleased]
-### Added
-- New configuration option `erbalist.highlightMode` to control Ruby highlighting behavior
-  - `always`: Always highlight Ruby code
-  - `whenInBlock`: Only highlight Ruby code within Ruby blocks
--- src/utils.ts
+++ src/utils.ts
@@ -1,51 +1,5 @@
 import * as vscode from 'vscode'
 
-// export function createToggleCommands(context: vscode.ExtensionContext, feature: string, callback: () => void) {
-//     ['on', 'off'].forEach(state => {
-//         context.subscriptions.push(
-//             vscode.commands.registerCommand(`erbalist.toggle${feature}.${state}`, callback)
-//         );
-//     });
-// }
-
-// export function setFeatureContext(feature: string, state: boolean) {
-//     return vscode.commands.executeCommand('setContext', `erbalist.${feature}Enabled`, state);
-// }
-
-// export function createDecorationStyle(color: string) {
-//     return vscode.window.createTextEditorDecorationType({
-//         backgroundColor: `rgba(${color}, 0.15)`,
-//         isWholeLine: true,
-//         borderRadius: '3px',
-//         overviewRulerColor: `rgba(${color}, 0.8)`,
-//         overviewRulerLane: vscode.OverviewRulerLane.Right,
-//         light: {
-//             backgroundColor: `rgba(${color}, 0.1)`,
-//         }
-//     });
-// }
-
-// export class RegexHelper {
-//     private regex: RegExp;
-    
-//     constructor(pattern: RegExp) {
-//         this.regex = new RegExp(pattern);
-//     }
-
-//     findMatches(text: string, callback: (match: RegExpExecArray) => void) {
-//         this.regex.lastIndex = 0;
-//         let match;
-//         while ((match = this.regex.exec(text)) !== null) {
-//             callback(match);
-//         }
-//     }
-
-//     test(text: string): boolean {
-//         this.regex.lastIndex = 0;
-//         return this.regex.test(text);
-//     }
-// }
-
 export function debounce<T extends (...args: any[]) => any>(
     func: T,
     wait: number
